1673730c2c932f2cfa3d007ef2b2def28a05451a
wissem. <wissem.badraoui@yahoo.com>
2025-02-28 15:08:13 +0100

    initialize Vue 3 + TypeScript + Vite project with Tailwind CSS and basic components

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Base from "./components/Base.vue";
+</script>
+
+<template>
+  <div class="align-center">
+      <img src="/wissem.svg" class="logo" alt="Wissem's Industries Logo" />
+  </div>
+  <Base msg="Hello World" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
