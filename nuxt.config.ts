1779f26a42317a587e2e26cb8a8e47ab6ea853dd
Wissem. <wissem.badraoui@yahoo.com>
2025-11-29 15:54:05 +0100

    fix: add experimental sqliteConnector configuration in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,10 @@ export default defineNuxtConfig({
 
 	css: ['~/assets/css/main.css'],
 
+	content: {
+		experimental: { sqliteConnector: 'native' }
+	},
+
 	runtimeConfig: {
 		smtpHost: process.env.SMTP_HOST,
 		smtpPort: process.env.SMTP_PORT,
