1e7440a6383516cc44db77a9fa252edf11f5f43a
Wissem. <contact@wissem.pro>
2026-03-10 13:57:40 +0100

    fix: adjust padding in HeroSection component

--- app/components/home/HeroSection.vue
+++ app/components/home/HeroSection.vue
@@ -34,7 +34,7 @@ function onDownloadResume() {
 </script>
 
 <template>
-	<div class="pt-18 sm:pt-16 pb-8 sm:pb-20">
+	<div class="pt-14 sm:pt-16 pb-8 sm:pb-20">
 		<UPageHero
 			:ui="{
 				headline: 'flex items-center justify-center',
